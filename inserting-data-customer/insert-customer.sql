6e673bd1280bdbf2db07d3a2ef57a66c9e63297a
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-09 07:17:39 +0700

    adding syntaks for inserting table products

---
+++ inserting-data-customer/insert-customer.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer 
+(name_customer, email, no_phone, address)
+VALUES 
+('Reza Arap Oktavian', 'gamers.ganteng.idaman@example.com', '+1 333-444-5555', 'Nakal Tapi Tampan');
+
+
+SELECT * FROM customer;
