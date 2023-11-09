3bbd2f40c1365a62639a90f39a6139bbc588ad8f
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-09 18:23:07 +0700

    resolving structure folder

---
+++ tables/table-payment-method/crud-payment-method/crud-payment-method.sql
@@ -0,0 +1,17 @@
+INSERT INTO payments_method
+(payment_method)
+VALUES
+('Wire Transfer');
+
+SELECT * FROM payments_method;
+
+
+-- For inserting Data
+-- 'Credit Card'
+-- 'Debit Card'
+-- 'PayPal'
+-- 'Bank Transfer'
+-- 'Cash on Delivery'
+-- 'Check'
+-- 'Money Order'
+-- 'Wire Transfer'
