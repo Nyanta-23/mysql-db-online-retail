800cb136e67df4357a92143325b2fa78a25dc0ab
Nyanta-23 <muhamadilhan02404@gmail.com>
2023-11-09 13:45:13 +0700

    adding query for inserting payment

---
+++ inserting-data-payment-method/inserting-payment-method.sql
@@ -0,0 +1,15 @@
+INSERT INTO payments_method
+(payment_method)
+VALUES
+('Wire Transfer');
+
+SELECT * FROM payments_method;
+
+'Credit Card'
+'Debit Card'
+'PayPal'
+'Bank Transfer'
+'Cash on Delivery'
+'Check'
+'Money Order'
+'Wire Transfer'
